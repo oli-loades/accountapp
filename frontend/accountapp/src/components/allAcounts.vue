ba4410e91be5364ab3f0dce9f6f625e1ea86095e
oli <oli.loades@gmail.com>
2018-09-02 15:37:39 +0100

    vuetify del acc

--- frontend/accountapp/src/components/allAcounts.vue
+++ frontend/accountapp/src/components/allAcounts.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="tableContainer">
     <v-app id="accountTable">
+
+      <v-alert
+        :value="!alert"
+        @click = 'alert = true'
+        color="success"
+        transition="scale-transition">
+        {{msg.message}}
+      </v-alert>
+
+      <v-dialog v-model="deleteDialog" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">Delete account?</v-card-title>
+          <v-card-text>Please confirm action.</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" flat @click.native="deleteDialog = false">Cancel</v-btn>
+            <v-btn color="green darken-1" flat @click.native="deleteAccount()">Confirm</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-container>
         <v-data-table :headers="headers" :items="accounts" hide-actions class="elevation-1">
           <template slot="items" slot-scope="props">
@@ -8,6 +28,14 @@
             <td class="text-xs-center">{{ props.item.firstname }}</td>
             <td class="text-xs-center">{{ props.item.surname }}</td>
             <td class="text-xs-center">{{ props.item.accountNumber }}</td>
+            <v-icon color='blue' meduim @click=''>edit</v-icon>
+            <v-spacer></v-spacer>
+            <v-icon color='red' meduim @click='deletePressed(props.item)'>delete</v-icon>
+          </template>
+          <template slot="no-data">
+            <v-alert :value="true" color="error" icon="warning">
+              No data to display.
+            </v-alert>
           </template>
         </v-data-table>
       </v-container>
@@ -22,16 +50,20 @@
     name: "allAcounts",
     data() {
       return {
+        deleteDialog: false,
+        toBeDeleted: 0,
+        msg: '',
+        alert: true,
         headers: [
           {
             text: 'ID',
             align: 'center',
-            sortable: false,
+            sortable: true,
             value: 'id'
           },
-          {text: 'First name', value: 'firstname', sortable: false,align: 'center'},
-          {text: 'Surname', value: 'surname', sortable: false,align: 'center'},
-          {text: 'Account number', value: 'accountNumber', sortable: false,align: 'center'},
+          {text: 'First name', value: 'firstname', sortable: true, align: 'center'},
+          {text: 'Surname', value: 'surname', sortable: true, align: 'center'},
+          {text: 'Account number', value: 'accountNumber', sortable: false, align: 'center'},
         ],
         accounts: []
       }
@@ -45,6 +77,25 @@
         .catch(e => {
           this.errors.push(e)
         })
+    },
+    methods: {
+      deleteAccount() {
+        console.log(this.toBeDeleted);
+        axios.delete('http://www.localhost:8182/account/delete/' + this.toBeDeleted.id)
+          .then(response => {
+            this.msg = response.data;
+            this.alert=false;
+            this.deleteDialog=false;
+            this.accounts.splice(this.accounts.indexOf(this.toBeDeleted), 1);
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      },
+      deletePressed(account){
+        this.toBeDeleted = account;
+        this.deleteDialog=true;
+      }
     }
   }
 </script>
--- frontend/accountapp/src/main.js
+++ frontend/accountapp/src/main.js
@@ -3,12 +3,8 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vuetify from 'vuetify'
 
-Vue.use(BootstrapVue);
 Vue.use(Vuetify);
 
 new Vue({
