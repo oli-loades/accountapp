e5eb156d497f6531e06eb6c39117683577068796
oli <oli.loades@gmail.com>
2018-09-02 16:09:54 +0100

    edit account

--- frontend/accountapp/src/components/allAcounts.vue
+++ frontend/accountapp/src/components/allAcounts.vue
@@ -4,7 +4,7 @@
 
       <v-alert
         :value="!alert"
-        @click = 'alert = true'
+        @click='alert = true'
         color="success"
         transition="scale-transition">
         {{msg.message}}
@@ -16,11 +16,37 @@
           <v-card-text>Please confirm action.</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="green darken-1" flat @click.native="deleteDialog = false">Cancel</v-btn>
-            <v-btn color="green darken-1" flat @click.native="deleteAccount()">Confirm</v-btn>
+            <v-btn color="black" flat @click.native="deleteDialog = false">Cancel</v-btn>
+            <v-btn color="black" flat @click.native="deleteAccount()">Confirm</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
+
+      <v-dialog v-model="editDialog" max-width="500px">
+        <v-card>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field v-model="editedAccount.firstname" label="First name"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field v-model="editedAccount.surname" label="Surname"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field v-model="editedAccount.accountNumber" label="Account number"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="black" flat @click="editDialog = false">Cancel</v-btn>
+              <v-btn color="black" flat @click="editAccount()">Confirm</v-btn>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
       <v-container>
         <v-data-table :headers="headers" :items="accounts" hide-actions class="elevation-1">
           <template slot="items" slot-scope="props">
@@ -28,15 +54,17 @@
             <td class="text-xs-center">{{ props.item.firstname }}</td>
             <td class="text-xs-center">{{ props.item.surname }}</td>
             <td class="text-xs-center">{{ props.item.accountNumber }}</td>
-            <v-icon color='blue' meduim @click=''>edit</v-icon>
+            <v-icon color='blue' meduim @click='editPressed(props.item)'>edit</v-icon>
             <v-spacer></v-spacer>
             <v-icon color='red' meduim @click='deletePressed(props.item)'>delete</v-icon>
           </template>
+
           <template slot="no-data">
             <v-alert :value="true" color="error" icon="warning">
               No data to display.
             </v-alert>
           </template>
+
         </v-data-table>
       </v-container>
     </v-app>
@@ -52,8 +80,10 @@
       return {
         deleteDialog: false,
         toBeDeleted: 0,
+        editDialog: false,
         msg: '',
         alert: true,
+        prevAcc: '',
         headers: [
           {
             text: 'ID',
@@ -65,7 +95,12 @@
           {text: 'Surname', value: 'surname', sortable: true, align: 'center'},
           {text: 'Account number', value: 'accountNumber', sortable: false, align: 'center'},
         ],
-        accounts: []
+        accounts: [],
+        editedAccount: {
+          firstname: '',
+          surname: '',
+          accountNumber: ''
+        }
       }
     },
     mounted: function () {
@@ -80,21 +115,45 @@
     },
     methods: {
       deleteAccount() {
-        console.log(this.toBeDeleted);
         axios.delete('http://www.localhost:8182/account/delete/' + this.toBeDeleted.id)
           .then(response => {
             this.msg = response.data;
-            this.alert=false;
-            this.deleteDialog=false;
+            this.alert = false;
+            this.deleteDialog = false;
             this.accounts.splice(this.accounts.indexOf(this.toBeDeleted), 1);
           })
           .catch(e => {
             this.errors.push(e)
           })
       },
-      deletePressed(account){
+      deletePressed(account) {
         this.toBeDeleted = account;
-        this.deleteDialog=true;
+        this.deleteDialog = true;
+      },
+      editAccount() {
+        axios.put('http://www.localhost:8182/account/update/' + this.prevAcc.id, this.editedAccount)
+          .then(response => {
+            this.msg = response.data;
+            this.alert = false;
+            this.editDialog = false;
+            this.update();
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      },
+      editPressed(account) {
+        this.prevAcc = account;
+        this.editedAccount.firstname = account.firstname;
+        this.editedAccount.surname = account.surname;
+        this.editedAccount.accountNumber = account.accountNumber;
+        this.editDialog = true;
+      },
+      update() {
+        const index = this.accounts.indexOf(this.prevAcc);
+        this.accounts[index].firstname = this.editedAccount.firstname;
+        this.accounts[index].surname = this.editedAccount.surname;
+        this.accounts[index].accountNumber = this.editedAccount.accountNumber;
       }
     }
   }
